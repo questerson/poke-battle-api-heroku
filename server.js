8e878e52a7857901130b98aa573bd0d93d8ddef0
questerson <yeadoneric@gmail.com>
2022-06-08 15:55:58 -0400

    pokemon app first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+const PORT = 8000
+
+// app.use(cors())
+
+
+
+class Pokemon {
+    constructor(name, type, imageUrl) {
+      this.name = name
+      this.type = type
+      this.imageUrl = imageUrl 
+    }
+  }
+  const pikachu = new Pokemon('Pikachu', 'Electric',  '/images/pika.png')
+  const bulbasaur = new Pokemon('Bulbasaur', 'Grass',  '/images/bulb.png')
+  const squirtle = new Pokemon('Squirtle', 'Water',  '/images/squirt.png')
+  const charmander = new Pokemon('Charmander', 'Fire', '/images/char.png')
+  const blastoise = new Pokemon('Blastoise', 'Water', '/images/blastoise.png')
+  const snorlax = new Pokemon('Snorlax', 'Normal',  '/images/snorlax.png')
+  const unknown = new Pokemon('Unknown', 'Unknown', '/images/unknown.png')
+  let pokemons = [pikachu, bulbasaur, squirtle, charmander, blastoise, snorlax, unknown]
+  
+
+
+  app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+
+
+app.get('/api/:name',(request,response)=>{
+    const pokeName = request.params.name.toLowerCase()
+
+   
+        response.json(pokemons)
+  
+    
+})
+
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+})
