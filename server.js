c33a67e58f0ff3d0a8582fd37ffd4a7040f5cfb3
questerson <yeadoneric@gmail.com>
2022-06-08 16:02:54 -0400

    pokemon app first commit

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ class Pokemon {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/images/bulb.png', (request, response)=>{
-    response.sendFile(__dirname + '/images/bulb.png')
-})
 
 
 app.get('/api/:name',(request,response)=>{
