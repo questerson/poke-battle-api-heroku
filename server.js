0549277a191f1bc36e74e9dc7ac212dd26db266e
questerson <yeadoneric@gmail.com>
2022-06-08 16:35:08 -0400

    tester

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 const PORT = 8000
 
-app.use(cors())
+// app.use(cors())
 
 
 
-class Pokemon {
-    constructor(name, type, imageUrl) {
-      this.name = name
-      this.type = type
-      this.imageUrl = imageUrl 
-    }
-  }
-  const pikachu = new Pokemon('Pikachu', 'Electric',  '/images/pika.png')
-  const bulbasaur = new Pokemon('Bulbasaur', 'Grass',  '/images/bulb.png')
-  const squirtle = new Pokemon('Squirtle', 'Water',  '/images/squirt.png')
-  const charmander = new Pokemon('Charmander', 'Fire', '/images/char.png')
-  const blastoise = new Pokemon('Blastoise', 'Water', '/images/blastoise.png')
-  const snorlax = new Pokemon('Snorlax', 'Normal',  '/images/snorlax.png')
-  const unknown = new Pokemon('Unknown', 'Unknown', '/images/unknown.png')
-  let pokemons = [pikachu, bulbasaur, squirtle, charmander, blastoise, snorlax, unknown]
+// class Pokemon {
+//     constructor(name, type, imageUrl) {
+//       this.name = name
+//       this.type = type
+//       this.imageUrl = imageUrl 
+//     }
+//   }
+//   const pikachu = new Pokemon('Pikachu', 'Electric',  '/images/pika.png')
+//   const bulbasaur = new Pokemon('Bulbasaur', 'Grass',  '/images/bulb.png')
+//   const squirtle = new Pokemon('Squirtle', 'Water',  '/images/squirt.png')
+//   const charmander = new Pokemon('Charmander', 'Fire', '/images/char.png')
+//   const blastoise = new Pokemon('Blastoise', 'Water', '/images/blastoise.png')
+//   const snorlax = new Pokemon('Snorlax', 'Normal',  '/images/snorlax.png')
+//   const unknown = new Pokemon('Unknown', 'Unknown', '/images/unknown.png')
+//   let pokemons = [pikachu, bulbasaur, squirtle, charmander, blastoise, snorlax, unknown]
   
 
 
